52c9e1ff40ec0ec3a212b0a82c597c6bbb3b0a59
praveenkumar <praveenpkbadiger@gmail.com>
2023-01-16 15:39:16 +0530

    Revert "added 3 files called names main and rollnu"
    This reverts commit 28641a1eebb8ad5f33f74b1306c78cb50754cf3d.

--- RollNu.java
+++ RollNu.java
@@ -1,6 +1,5 @@
 public class RollNu {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.printf("this is roll number program");
     }
 }
