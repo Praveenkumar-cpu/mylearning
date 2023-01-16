2dc5d240eddc0c922b44e2f8c8128ecf16d62316
praveenkumar <praveenpkbadiger@gmail.com>
2023-01-16 18:11:31 +0530

    Revert "added one thing"title
    This reverts commit a4042821b6bea4c721cfa133e90775e9d580d118.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
-        System.out.printf("Added somthing in names and rollnuß");
-        System.out.printf("Add one change");
+        System.out.printf("Added in names and rollnu");
+        System.out.printf("Change Number one");
     }
 }
