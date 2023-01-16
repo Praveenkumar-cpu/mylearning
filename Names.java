7b98343caa09c6fd598889414ad1558df0bee269
praveenkumar <praveenpkbadiger@gmail.com>
2023-01-16 15:52:14 +0530

    added: praveen  added some content in Names and RollNu

--- Names.java
+++ Names.java
@@ -1,5 +1,6 @@
 public class Names {
     public static void main(String[] args) {
         System.out.printf("this is names program");
+        System.out.printf("first name is praveen");
     }
 }
--- RollNu.java
+++ RollNu.java
@@ -1,5 +1,6 @@
 public class RollNu {
     public static void main(String[] args) {
         System.out.printf("this is roll number program");
+        System.out.printf("my roll number 3434");
     }
 }
