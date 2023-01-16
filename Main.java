4b8b7d72a76bca334f1ddaba92c682892278c953
praveenkumar <praveenpkbadiger@gmail.com>
2023-01-16 15:54:16 +0530

    added: somthing is main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+        System.out.printf("Added somthing in names and rollnuß");
     }
 }
