85d76e4cfe9daaca9226b3ad61f5c32b869a58dc
praveenkumar <praveenpkbadiger@gmail.com>
2023-01-16 15:58:17 +0530

    Revert "added: second names and roll number is added"
    This reverts commit 56943bd979e1a022a87bd5eee78d435266567575.

--- RollNu.java
+++ RollNu.java
@@ -2,6 +2,5 @@ public class RollNu {
     public static void main(String[] args) {
         System.out.printf("this is roll number program");
         System.out.printf("my roll number 3434");
-        System.out.printf("arun roll num is 4545");
     }
 }
